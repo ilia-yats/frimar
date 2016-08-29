7e4cc5287f4eddb2396012a0501ab00dc64b1e09
ilia <ilia-yats@ukr.net>
2016-08-29 22:31:47 +0300

    db connection fixed to right database

--- frimar_dump.sql
+++ frimar_dump.sql
@@ -1,29 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10.10
--- http://www.phpmyadmin.net
---
--- Хост: 127.0.0.1:3306
--- Время создания: Авг 29 2016 г., 04:19
--- Версия сервера: 5.5.45
--- Версия PHP: 5.5.28
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- База данных: `frimar`
---
-CREATE DATABASE IF NOT EXISTS `frimar` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `frimar`;
-
--- --------------------------------------------------------
-
 --
 -- Структура таблицы `contacts`
 --
@@ -138,7 +112,3 @@ ALTER TABLE `contacts`
 --
 ALTER TABLE `streets`
   ADD CONSTRAINT `fk_streets_districts` FOREIGN KEY (`district_id`) REFERENCES `districts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
